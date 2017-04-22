f29427ac830abebde960e0d6b1b9914a3d83836e
Rusheel Shah <rusheelshah@rusheels-macbook-pro.local>
2017-04-22 18:46:17 -0400

    Initial Commit

---
+++ TeamPulseApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TeamPulseApp
+//
+//  Created by Rusheel Shah on 4/22/17.
+//  Copyright © 2017 Rusheel Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
